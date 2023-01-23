665f7016f16f007e9bfda807742f194db918e8c3
atuld <atuld@DESKTOP-C940CC5>
2023-01-23 20:08:26 +0530

    atul 23012023 v1526

---
+++ springAWS/src/main/java/com/aws/controller/AwsController.java
@@ -0,0 +1,15 @@
+package com.aws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AwsController {
+	
+	@GetMapping("/rest")
+	public String home() {
+		return "AWS first deployment...";
+		
+	}
+
+}
